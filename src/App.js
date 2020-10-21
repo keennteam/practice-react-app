31b4526793466efa427f65d2a73547b1540e04b0
bitfede <f.g.defaveri@gmail.com>
2020-10-21 18:47:54 -0400

    Fixes vulns by updating package

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Federico from './Components/Federico/Federico'
+import ClassComponent from './Components/ClassComponent/ClassComponent'
 import FxComponent from './Components/FxComponent/FxComponent'
 
 function App() {
 
-  console.log(window.location);
+  const appID = "PracticeApp";
 
   return (
     <div className="App">
       <header className="App-header">
 
+
         <FxComponent />
 
+        <ClassComponent applicationID={appID} />
 
       </header>
     </div>
